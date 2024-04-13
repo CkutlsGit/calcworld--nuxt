b8f05f06d8cc2b4e857bd86181084b1b55db5653
Kostya <ckutls228@gmail.com>
2024-04-13 08:52:46 +0300

    added header-component style

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header bg-header">
+    <div class="header__content flex items-center justify-between p-4">
+      <div class="header__logo m-4">
+        <h1 class="text-3xl color-text-custom font-merriweather font-bold">Calc-World</h1>
+      </div>
+      <div class="header__nav m-4">
+        <div class="nav-menu relative inline-block">
+          <button class="text-lg font-montserrat font-semibold text-gray-50 bg-btns p-2 rounded-lg">Калькуляторы</button>
+          <div class="nav-menu__content flex flex-col text-center bg-header-menu rounded-lg w-36 max-h-64 overflow-y-auto absolute opacity-0">
+            <NuxtLink class="btn-header-menu">Обычный калькулятор</NuxtLink>
+            <NuxtLink class="btn-header-menu">Обычный калькулятор</NuxtLink>
+            <NuxtLink class="btn-header-menu">Обычный калькулятор</NuxtLink>
+            <NuxtLink class="btn-header-menu">Обычный калькулятор</NuxtLink>
+            <NuxtLink class="btn-header-menu">Обычный калькулятор</NuxtLink>
+            <NuxtLink class="btn-header-menu">Обычный калькулятор</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.nav-menu:hover button {
+  background: #c1c7a2;
+}
+.nav-menu:hover .nav-menu__content {
+  opacity: 1;
+}
+.nav-menu__content {
+  transition: opacity .2s ease-in;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,11 @@
 <template>
-  <h1 class="text-center font-merriweather uppercase text-4xl text-green-700 font-bold">test</h1>
+  <HeaderComponent/>
   <div>
     <slot/>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
